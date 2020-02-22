a489888a60a3a22c8edcc769b8f5c476ad8e66f0
David Lane <fastlane27@gmail.com>
2020-02-21 18:28:11 -0600

    Update styling

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const methodOverride = require('method-override');
 const indexRouter = require('./routes/index');
 const skillsRouter = require('./routes/skills');
+
+const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use('/skills', skillsRouter);
